993b60c30c84226c447f64231f1d02ec644a908f
Chad Nedzlek <chadnedz@microsoft.com>
2019-09-05 12:08:15 -0700

    Try adding some logging
    We are getting a lot of "400 Bad Request" responses on this server
    but the logs don't show why. This filter appears to be the culprit, so
    add logging to test that hypothesis

--- src/ValidateModelStateAttribute.cs
+++ src/ValidateModelStateAttribute.cs
@@ -2,23 +2,64 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Linq;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.DotNet.HelixPoolProvider
 {
-    public class ValidateModelStateAttribute : ActionFilterAttribute
+    public class ValidateModelStateAttribute : TypeFilterAttribute
     {
-        public ValidateModelStateAttribute()
+        public ValidateModelStateAttribute() : base(typeof(ValidateModelStateImpl))
         {
             Order = int.MaxValue;
         }
 
-        public override void OnActionExecuting(ActionExecutingContext context)
+        private class ValidateModelStateImpl : IActionFilter
         {
-            if (!context.ModelState.IsValid)
+            private readonly ILogger<ValidateModelStateAttribute> _logger;
+
+            public ValidateModelStateImpl(ILogger<ValidateModelStateAttribute> logger)
             {
-                context.Result = new BadRequestObjectResult(context.ModelState);
+                _logger = logger;
+            }
+
+            public void OnActionExecuting(ActionExecutingContext context)
+            {
+                if (!context.ModelState.IsValid)
+                {
+                    LogValidationFailures(context);
+
+                    context.Result = new BadRequestObjectResult(context.ModelState);
+                }
+            }
+
+            private void LogValidationFailures(ActionContext context)
+            {
+                StringBuilder errorString = new StringBuilder();
+                foreach (var (prop, entry) in context.ModelState)
+                {
+                    if (entry.Errors.Count > 0)
+                    {
+                        if (errorString.Length != 0)
+                        {
+                            errorString.Append(" | ");
+                        }
+
+                        errorString.Append(prop);
+                        errorString.Append(" : ");
+
+                        errorString.AppendJoin(", ", entry.Errors.Select(e => e.ErrorMessage));
+                    }
+                }
+
+                _logger.LogWarning("Invalid view state detected: {message}", errorString.ToString());
+            }
+
+            public void OnActionExecuted(ActionExecutedContext context)
+            {
             }
         }
     }
