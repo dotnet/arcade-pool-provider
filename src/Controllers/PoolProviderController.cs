1028ba93b7928031e1e2ac1e67ddd5997f1d9e14
Matt Galbraith <MattGal@users.noreply.github.com>
2020-04-16 13:03:31 -0700

    Improve pool provider status messages
    Remove " or does not exist." as well as if the work was submitted to the point of having a correlation id, the work exists

--- src/Controllers/PoolProviderController.cs
+++ src/Controllers/PoolProviderController.cs
@@ -376,17 +376,17 @@ namespace Microsoft.DotNet.HelixPoolProvider.Controllers
                 case "unscheduled":
                     return Json(new AgentStatusItem()
                     {
-                        statusMessage = $"Helix work item in job {correlationId} for agent {workItemId} is currently {workItemDetails.State} or does not exist."
+                        statusMessage = $"(Work has been submitted to the Helix API, but has not started yet) Helix work item in job {correlationId} for agent {workItemId} is currently {workItemDetails.State}"
                     });
                 case "waiting":
                     return Json(new AgentStatusItem()
                     {
-                        statusMessage = $"Helix work item in job {correlationId} for agent {workItemId} is currently waiting for a machine."
+                        statusMessage = $"(Work has been submitted to the Helix API, and is waiting for a machine) Helix work item in job {correlationId} for agent {workItemId} is currently waiting for a machine."
                     });
                 case "failed":
                     return Json(new AgentStatusItem()
                     {
-                        statusMessage = $"Helix work item in job {correlationId} for agent {workItemId} failed.  Please check the logs."
+                        statusMessage = $"(Error state: Please contact dnceng with Job Id) Helix work item in job {correlationId} for agent {workItemId} failed.  Please check the logs."
                     });
                 default:
                     throw new NotImplementedException(
