a483f1dbf60a2d4871c9b8cbe425654a16cabede
Lukasz Patalas <lupatala@microsoft.com>
2021-04-15 17:10:57 +0200

    Add test project

---
+++ tests/TestTest.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Microsoft.DotNet.HelixPoolProvider.Tests
+{
+    public class TestTest
+    {
+        [Fact]
+        public void RunTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
